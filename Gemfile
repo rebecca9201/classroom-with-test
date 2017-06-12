49a98245ade9f08c16ed669de640fa0dd94848d4
rebecca9201 <wangmmqq@gmail.com>
2017-06-12 18:18:09 +0800

    user must sign in to create course

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'devise', '~> 4.2.0'
+gem 'simple_form', '~> 3.3.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -40,20 +40,34 @@ RSpec.describe CoursesController do
   end
 
   describe "GET new" do
-    it "assign @course" do
-      course = build(:course)
+    context "when user login" do
+      it "assigns @course" do
+        user = create(:user)
+        course = build(:course)
 
-      get :new
+        sign_in user
+        get :new
 
-      expect(assigns(:course)).to be_a_new(Course)
+        expect(assigns(:course)).to be_a_new(Course)
+      end
+
+      it "renders template" do
+        user = create(:user)
+        course = build(:course)
+
+        sign_in user
+        get :new
+
+        expect(response).to render_template("new")
+      end
     end
 
-    it "render template" do
-      course = build(:course)
+    context "when user not login" do
+      it "redirect_to new_user_session_path" do
+        get :new
 
-      get :new
-
-      expect(response).to render_template("new")
+        expect(response).to redirect_to new_user_session_path
+      end
     end
   end
 
