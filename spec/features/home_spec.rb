d4a571f18f30924142e8cfda6a24794f7bd29d73
rebecca9201 <wangmmqq@gmail.com>
2017-06-12 20:07:36 +0800

    增加登录登出测试

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,9 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "Home" do
+  scenario "welcomes user" do
+    visit root_url
+    expect(page).to have_text "Welcome"
+  end
+
   scenario "has navbar element" do
     visit root_url
-
     expect(page).to have_css "nav.navbar"
   end
 end
