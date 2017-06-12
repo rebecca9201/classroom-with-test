4411dddea03e750e760245fc7a5212370d75247c
rebecca9201 <wangmmqq@gmail.com>
2017-06-12 16:03:50 +0800

    补上show的测试

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,15 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe CoursesController, type: :controller do
+RSpec.describe CoursesController do
   describe "GET index" do
-   it "assigns @courses and render template" do
-     course1 = Course.create(title: "foo", description: "bar")
-     course2 = Course.create(title: "bar", description: "foo")
+    it "assigns @courses and render" do
+     course1 = create(:course)
+     course2 = create(:course)
 
      get :index
 
      expect(assigns[:courses]).to eq([course1, course2])
+   end
+
+   it "render template" do
+     course1 = create(:course)
+     course2 = create(:course)
+
+     get :index
+
      expect(response).to render_template("index")
    end
- end
+  end
+
+  describe "GET show" do
+    it "assigns @course" do
+      course = create(:course)
+
+      get :show, params: { id: course.id }
+
+      expect(assigns[:course]).to eq(course)
+    end
+
+    it "render template" do
+      course = create(:course)
+
+      get :show, params: { id: course.id }
+
+      expect(response).to render_template("show")
+    end
+  end
 end
